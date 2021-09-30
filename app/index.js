1fb56fcc8e35bef6b445efe86c4c12faa131b54c
Logan Butler <logan.butler@student.swic.edu>
2021-09-29 20:23:18 -0500

    Work on switch cases.

--- app/index.js
+++ app/index.js
@@ -1,21 +1,21 @@
-// Specifically import the promises part of the fs module
-// For convenience, ren it to fs
+import express from "express";
 import { promises as fs } from "fs";
-import http from "http";
-const port = 3000;
-let count = 0;
-// Create the initial file
-fs.writeFile("count.log", "");
-const server = http.createServer((_, res) => {
-  res.statusCode = 200; // Status code - OK
-  // Server announces to the browser what type of stuff is in this response - HTML
-  res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Goodbye World</h1>");
-  count += 1;
-  console.log("Incrementing");
 
-  fs.writeFile("count.log", String(count));
+const app = express();
+
+app.get("/:page", (req, res) => {
+  switch (req.params.page) {
+    case "hello":
+      res.end("Hello");
+      break;
+    case "bye":
+      res.end("bye");
+      break;
+    default:
+      res.end("404");
+  }
 });
-server.listen(port, () => {
-  console.info(`Server is running: http://localhost:${port}`);
+
+app.listen(3000, () => {
+  console.info("Server running");
 });
