2fccd090ca4daa10b72fe7d3974582bc665bd0b2
Logan Butler <logan.butler@student.swic.edu>
2021-09-29 19:35:30 -0500

    Update code with teachers code

--- app/index.js
+++ app/index.js
@@ -1,16 +1,20 @@
+// Specifically import the promises part of the fs module
+// For convenience, ren it to fs
+import { promises as fs } from "fs";
 import http from "http";
-
 const port = 3000;
-
-const server = http.createServer((req, res) => {
-  console.log(req);
-  res.statusCode = 200; // Status code = OK
-
+let count = 0;
+// Create the initial file
+fs.writeFile("count.log", "");
+const server = http.createServer((_, res) => {
+  res.statusCode = 200; // Status code - OK
   // Server announces to the browser what type of stuff is in this response - HTML
   res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello World</h1>");
-});
+  res.end("<h1>Goodbye World</h1>");
+  count += 1;
 
+  fs.writeFile("count.log", String(count));
+});
 server.listen(port, () => {
   console.info(`Server is running: http://localhost:${port}`);
 });
